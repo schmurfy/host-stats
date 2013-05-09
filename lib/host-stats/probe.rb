fc87a41004e8099f691bb13f4eaed7deafa4ac77
Julien Ammous <schmurfy@gmail.com>
2013-05-09 12:07:56 +0200

    removed freebsd only apis

--- lib/host-stats/probe.rb
+++ lib/host-stats/probe.rb
@@ -1,27 +1,15 @@
 module HostStats
   
-  module LibC
-    extend FFI::Library
-    ffi_lib 'c'
+  # module LibC
+  #   extend FFI::Library
+  #   ffi_lib 'c'
     
-    attach_function :sysctlbyname, [:string, :pointer, :pointer, :pointer, :size_t], :int
-  end
+  # end
   
   ##
   # This class provides the base functions required to write a probe
   # 
   class Probe
-    def read_sysctl(name, expected_return = :int)
-      out = FFI::MemoryPointer.new(expected_return)
-      len = FFI::MemoryPointer.new(:size_t)
-      len.write_int(out.size)
-      
-      check_error!(
-          LibC.sysctlbyname(name, out, len, nil, 0)
-        )
-      
-      out.send("read_#{expected_return}")
-    end
     
     # private
       def check_error!(ret)
